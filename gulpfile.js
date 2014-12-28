13cc2ecd5d2ebd67fb30e81a35d0c13c91e2d79d
ablyakim <ablylimov@gmail.com>
2014-12-28 19:26:46 +0300

    gulp build css

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,7 @@
-var less = require('gulp-less');
-var path = require('path');
+
 var gulp = require('gulp');
-var minifyCSS = require('gulp-minify-css');
+
 var argv = require('yargs').argv;
+var defaultBundleCssTasks = require('./src/Bundles/DefaultBundle/Resources/public/gulp_tasks_css.js')(gulp,argv);
 
-gulp.task('less', function () {
-    gulp.src('./bower_components/bootstrap/less/bootstrap.less')
-        .pipe(less({
-            paths: [ './bower_components/bootstrap/less/bootstrap.less' ]
-        }))
-        .pipe(gulp.dest('./web/build/css'));
-});
-
-gulp.task('minify-css', function() {
-    gulp.src('./web/build/css/*.css')
-        .pipe(minifyCSS({keepBreaks:true}))
-        .pipe(gulp.dest('./web/build/css/dist/'))
-});
-
-
-gulp.task('default',['less','minify-css']);
+gulp.task('default',defaultBundleCssTasks);
---
+++ src/Bundles/DefaultBundle/Resources/public/gulp_tasks_css.js
@@ -0,0 +1,33 @@
+var minifyCSS = require('gulp-minify-css');
+var less = require('gulp-less');
+
+module.exports = function(gulp,argv){
+
+    gulp.task('default_bundle_less', function () {
+        gulp.src('./bower_components/bootstrap/less/bootstrap.less')
+            .pipe(less({
+                paths: [ './bower_components/bootstrap/less/bootstrap.less' ]
+            }))
+            .pipe(gulp.dest('./web/build/css'))
+            .pipe(minifyCSS({keepBreaks:true}))
+            .pipe(gulp.dest('./web/build/dist/css'));
+    });
+
+
+    gulp.task('default_bundle_minify-css', function() {
+        gulp.src([
+            './src/Bundles/DefaultBundle/Resources/public/css/avia.css',
+            './src/Bundles/DefaultBundle/Resources/public/lib/ui-theme/css/jquery-ui.css'])
+            .pipe(minifyCSS({keepBreaks:true}))
+            .pipe(gulp.dest('./web/build/dist/css'));
+
+        gulp.src(['./src/Bundles/DefaultBundle/Resources/public/images/**/*'])
+            .pipe(gulp.dest('./web/build/dist/images'));
+    });
+    if(argv.dev){
+        gulp.watch(['./src/Bundles/DefaultBundle/Resources/public/css/*.css'],['default_bundle_minify-css']);
+    }
+
+    return ['default_bundle_less','default_bundle_minify-css'];
+
+};
