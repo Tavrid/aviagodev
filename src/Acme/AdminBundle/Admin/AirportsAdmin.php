c86672931fe88e2646a00470ac0af9e0453dc630
ablyakim <ablylimov@gmail.com>
2015-01-27 21:46:42 +0300

    work

---
+++ src/Acme/AdminBundle/Admin/AirportsAdmin.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ablyakim
+ * Date: 27.01.15
+ * Time: 21:31
+ */
+
+namespace Acme\AdminBundle\Admin;
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class AirportsAdmin extends Admin {
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('id')
+            ->add('countryRus')
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('countryRus')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->addIdentifier('cityRus')
+            ->add('countryRus')
+            ->add('cityCodeEng')
+            ->add('airportCodeEng')
+            ->add('nameShortRu',null,['editable' => true,'label' => 'Короткое название(rus)'])
+            ->add('nameShortEn',null,['editable' => true,'label' => 'Короткое название(en)'])
+            ->add('nameShortUk',null,['editable' => true,'label' => 'Короткое название(uk)'])
+        ;
+    }
+
+}
