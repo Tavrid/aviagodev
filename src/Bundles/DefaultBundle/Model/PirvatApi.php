4882c3f3114107ed964c4c2c2d90960582280d82
ablyakim <ablylimov@gmail.com>
2015-02-26 22:03:06 +0300

    privat

--- src/Bundles/DefaultBundle/Model/PirvatApi.php
+++ src/Bundles/DefaultBundle/Model/PirvatApi.php
@@ -14,22 +14,39 @@ use Acme\AdminBundle\Entity\Order;
 class PirvatApi {
     protected $paiUrl = 'https://api.privatbank.ua/p24api/ishop';
 
+    protected $merchant;
+    protected $merchantPass;
+    protected $returnUrl;
+
+    public function __construct($merchant,$merchantPass){
+        $this->merchant = $merchant;
+        $this->merchantPass = $merchantPass;
+    }
+
     public function createForm(Order $order){
 
+        $formData = [
+          'amt' => '15.25',//test
+            'ccy' => 'UAH',
+            'merchant' => trim($this->merchant),
+            'order' => '12', //test
+            'details' => 'details test',
+            'ext_details' => '',//test
+            'pay_way' => 'privat24',
+            'return_url' => $this->returnUrl,
+            'server_url' => '',
+        ];
 
-        return '<form action="https://api.privatbank.ua/p24api/ishop" method="POST" accept-charset="UTF-8">
-                  <input type="hidden" name="amt" value="15.25"/>
-                  <input type="hidden" name="ccy" value="UAH" />
-                  <input type="hidden" name="merchant" value="75482" />
-                  <input type="hidden" name="order" value="000AB1502ZGH" />
-                  <input type="hidden" name="details" value="книга Будь здоров!" />
-                  <input type="hidden" name="ext_details" value="1000BDN01" />
-                  <input type="hidden" name="pay_way" value="privat24" />
-                  <input type="hidden" name="return_url" value="https://mysite.net/papi/rurl.php" />
-                  <input type="hidden" name="server_url" value="" />
-                  <input type="hidden" name="signature" value="3A90E5J0f6OUIfqN1Qu59gYrjDgDblfL" />
-                  <input type="submit" value="Оплатить" />
-                </form>';
+
+        $sign = $this->generateSignature($formData,$this->merchantPass);
+        $formData['signature'] = $sign;
+
+        $str =  '<form action="https://api.privatbank.ua/p24api/ishop" method="POST" accept-charset="UTF-8">';
+        foreach ($formData as $key => $val){
+            $str.=sprintf('<input type="hidden" name="%s" value="%s"/>',$key,$val);
+        }
+        $str .='</form>';
+        return $str;
     }
 
     /**
@@ -43,7 +60,7 @@ class PirvatApi {
         foreach ($params as $par){
             $str.=$formData[$par];
         }
-        return sha1(md5($str.$pass));
+        return sha1(md5($str.trim($pass)));
 
     }
 }
