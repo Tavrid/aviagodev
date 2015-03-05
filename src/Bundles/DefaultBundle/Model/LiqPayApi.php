d930625cb57a6fe3cc0032d6c219d29e3bfeef6b
ablyakim <ablylimov@gmail.com>
2015-03-05 23:27:32 +0300

    work

--- src/Bundles/DefaultBundle/Model/LiqPayApi.php
+++ src/Bundles/DefaultBundle/Model/LiqPayApi.php
@@ -11,25 +11,29 @@ namespace Bundles\DefaultBundle\Model;
 
 use Acme\AdminBundle\Entity\Order;
 
-class LiqPayApi {
+class LiqPayApi
+{
 
     protected $publicKey;
     protected $privateKey;
 
-    public function __construct($publicKey,$privateKey){
+    public function __construct($publicKey, $privateKey)
+    {
         $this->publicKey = $publicKey;
         $this->privateKey = $privateKey;
     }
 
-    public function createForm(Order $order){
+    public function createForm(Order $order)
+    {
 
         $liqpay = new \LiqPay($this->publicKey, $this->privateKey);
         $html = $liqpay->cnb_form(array(
-            'version'        => '3',
-            'amount'         => $order->getPrice(),
-            'currency'       => 'RUB',
-            'description'    => 'Order',
-            'order_id'       => $order->getId()
+            'version' => '3',
+            'amount' => $order->getPrice(),
+            'currency' => 'RUB',
+            'description' => 'Order',
+            'order_id' => $order->getId(),
+            'sandbox' => 1
         ));
         return $html;
     }
