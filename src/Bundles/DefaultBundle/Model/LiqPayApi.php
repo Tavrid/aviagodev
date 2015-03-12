6ff5da48206880eaf1ee462f974ebd5214df4d66
ablyakim <ablylimov@gmail.com>
2015-03-12 20:07:52 +0300

    work

--- src/Bundles/DefaultBundle/Model/LiqPayApi.php
+++ src/Bundles/DefaultBundle/Model/LiqPayApi.php
@@ -10,6 +10,8 @@ namespace Bundles\DefaultBundle\Model;
 
 
 use Acme\AdminBundle\Entity\Order;
+use Bundles\ApiBundle\Api\Response\BookInfoResponse;
+use Bundles\ApiBundle\Api\Util\TicketEntityCreator;
 use Symfony\Component\Routing\RouterInterface;
 
 class LiqPayApi
@@ -32,7 +34,11 @@ class LiqPayApi
 
     public function createForm(Order $order)
     {
-
+        //{"originCity":"DP","destinationCity":"NY","departureDate":"100514"}
+        $bookInfoResponse = new BookInfoResponse(new TicketEntityCreator());
+        $bookInfoResponse->setResponseData($order->getOrderInfo());
+        $ticket = $bookInfoResponse->getEntity()->getTicket();
+        $segment = $ticket->getFirstItinerarie()->getFirstVariant()->getDepartureSegment();
         $liqpay = new \LiqPay($this->publicKey, $this->privateKey);
         $html = $liqpay->cnb_form(array(
             'version' => '3',
@@ -41,6 +47,16 @@ class LiqPayApi
             'description' => 'Order',
             'order_id' => $order->getId(),
             'sandbox' => 1,
+            'dae' => json_encode([
+                'airLine' => $segment->getMarketingAirlineName(),
+                'ticketNumber' => $order->getPnr(),
+                'passengerName' => $ticket->getSurname(),
+                'flightNumber' => $segment->getFlightNumber(),
+                'originCity' => $segment->getDepartureCity(),
+                'destinationCity' => $segment->getDepartureCityName(),
+                'departureDate' => $segment->getDepartureDate()
+            ]),
+            'server_url' => 'http://webservices.aero/',
             'result_url' => $this->router->generate('bundles_default_api_return_liqpay',array("orderID" => $order->getOrderId()),true)
         ));
         return $html;
@@ -53,6 +69,9 @@ class LiqPayApi
             'order_id'      => $order->getId()
         ));
         if($res){
+            if($res->result == 'error'){
+                return false;
+            }
             if($res->order_id == $order->getId() && $res->amount == $order->getPrice() && $res->status == self::SUCCESS_PAY){
                 return true;
             }
