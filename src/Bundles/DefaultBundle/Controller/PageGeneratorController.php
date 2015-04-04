45f619b714369d210486b5e5d015e14ccd7c184e
Ablyakim Ablyalimov <ablylimov@gmail.com>
2015-04-04 20:05:49 +0300

    work

--- src/Bundles/DefaultBundle/Controller/PageGeneratorController.php
+++ src/Bundles/DefaultBundle/Controller/PageGeneratorController.php
@@ -14,12 +14,26 @@ class PageGeneratorController extends Controller
         if(!$data){
             throw $this->createNotFoundException('Error parse uri');
         }
+
         $twig = clone $this->get('twig');
         $twig->setLoader(new \Twig_Loader_String());
         $rendered = $twig->render(
             $data->getTemplate(),
             array("data" => $data)
         );
+        $metaTags = [
+            'title' => '',
+            'keywords' => '',
+            'description' => ''
+        ];
+        if(is_array($data->getMetaTags())){
+            foreach ($data->getMetaTags() as $k => $v){
+                $metaTags[$k] = $twig->render(
+                    $v,
+                    array("data" => $data)
+                );
+            }
+        }
 
         $h1 = $twig->render(
             $data->getH1(),
@@ -31,7 +45,8 @@ class PageGeneratorController extends Controller
             'form_data' => [],
             'form' => $form = $this->createForm('search_form')->createView(),
             'content' => $rendered,
-            'h1' => $h1
+            'h1' => $h1,
+            'metaTags' => $metaTags
         ]);
     }
 }
