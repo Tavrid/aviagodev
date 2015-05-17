453d84bca8d656e42833317266af507f034ae0d1
Ablyakim Ablyalimov <ablylimov@gmail.com>
2015-05-17 18:13:23 +0300

    currency manager

--- src/Bundles/ApiBundle/Api/Price/PriceResolver.php
+++ src/Bundles/ApiBundle/Api/Price/PriceResolver.php
@@ -11,23 +11,33 @@ namespace Bundles\ApiBundle\Api\Price;
 
 use Bundles\ApiBundle\Api\Entity\Ticket;
 use Bundles\ApiBundle\Api\Query\QueryAbstract;
+use Bundles\DefaultBundle\Util\CurrencyManager;
 
 class PriceResolver implements PriceResolverInterface
 {
+    /**
+     * @var CurrencyManager
+     */
+    protected $currencyManager;
+
+    /**
+     * @param CurrencyManager $currencyManager
+     */
+    public function __construct(CurrencyManager $currencyManager)
+    {
+        $this->currencyManager = $currencyManager;
+    }
     /**
      * @inheritdoc
      */
     public function resolve($response,QueryAbstract $query = null)
     {
-        $currency = 'RUB';
-        if(!is_null($query)){
-            $currency = strtoupper($query->getParam('currency'));
-        }
+        $currency = $this->currencyManager->getCurrency();
         $prices = [];
         if(isset($response['TotalPrice'])){
             $prices[$response['TotalPrice']['Currency']] = $response['TotalPrice'];
         } else {
-            $prices['RUB'] = ['Total' => null];
+            $prices[$currency] = ['Total' => null];
         }
 
         if(isset($response['TotalPriceConverted'])){
@@ -37,7 +47,7 @@ class PriceResolver implements PriceResolverInterface
         }
 
         if(!isset($prices[$currency])){
-            $currency = 'RUB';
+            return ['price' => ['Total' => null],'currency' => $currency];
         }
 
         return ['price' => $prices[$currency],'currency' => $currency];
