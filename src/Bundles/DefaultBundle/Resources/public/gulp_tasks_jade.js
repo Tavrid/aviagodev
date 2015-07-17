eb27b456012f18a0bd916bdc0071672624a1a89e
Ablyakim <ablylimov@gmail.com>
2015-07-18 01:05:03 +0300

    gulp minify html

--- src/Bundles/DefaultBundle/Resources/public/gulp_tasks_jade.js
+++ src/Bundles/DefaultBundle/Resources/public/gulp_tasks_jade.js
@@ -2,11 +2,18 @@ var gulp = require('gulp');
 var argv = require('yargs').argv;
 var path = require('path');
 var jade = require('gulp-jade');
+var minifyHTML = require('gulp-minify-html');
 module.exports = function(){
+
+    var opts = {
+        conditionals: true,
+        spare:true
+    };
 
     gulp.task('default_jade', function () {
         gulp.src(path.join(__dirname, 'src/jade/**/*.jade'))
             .pipe(jade())
+            .pipe(minifyHTML(opts))
             .pipe(gulp.dest('./web/build/view'));
     });
 
